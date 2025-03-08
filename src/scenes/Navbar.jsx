9a0415bed945f8a3301e398bdc04a48a5b051905
Saurabh Pandey <saurabh.k.pandey2018@gmail.com>
2025-03-08 16:18:51 +0530

    nav

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
   const navbarBackground = isTopOfPage ? "" : "bg-red";
 
   return (
-    <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
+    <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-2`}>
       <div className="flex items-center justify-between mx-auto w-5/6">
         <h4 className="font-playfair text-3xl font-bold">Saurabh</h4>
 
